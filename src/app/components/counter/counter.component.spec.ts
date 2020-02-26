9fe93449204cb38bbf52fb3e2549af733adbb1e6
isma-baena <ishman.cbre@gmail.com>
2020-02-26 11:23:51 +0100

    Config jest / testing library works

---
+++ src/app/components/counter/counter.component.spec.ts
@@ -0,0 +1,34 @@
+import 'jest';
+import { render } from '@testing-library/angular';
+import { CounterComponent } from './counter.component';
+
+describe('Counter', () => {
+  test('should render counter', async () => {
+    const { getByText } = await render(CounterComponent, {
+      componentProperties: { counter: 5 },
+    });
+
+    expect(getByText('Current Count: 5'));
+  });
+
+  test('should increment the counter on click', async () => {
+    const { getByText, click } = await render(CounterComponent, {
+      componentProperties: { counter: 5 },
+    });
+
+    click(getByText('+'));
+
+    expect(getByText('Current Count: 6'));
+  });
+
+  test('should decrement the counter on click', async () => {
+    const { getByText, click } = await render(CounterComponent, {
+      componentProperties: { counter: 5 },
+    });
+
+    click(getByText('-'));
+
+    expect(getByText('Current Count: 4'));
+  });
+
+})
---
+++ src/app/components/counter/counter.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'counter',
+  template: `
+    <button (click)="decrement()">-</button>
+    <span data-testid="count">Current Count: {{ counter }}</span>
+    <button (click)="increment()">+</button>
+  `,
+})
+export class CounterComponent {
+  @Input() counter = 0
+
+  increment() {
+    this.counter += 1
+  }
+
+  decrement() {
+    this.counter -= 1
+  }
+}
